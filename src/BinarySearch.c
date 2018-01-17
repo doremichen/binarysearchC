c3d1b4dd0903a60e374646cadafb76982d473ffc
AdamChen <mnre2003@yahoo.com.tw>
2018-01-17 16:52:14 +0800

    add buble sort flow and refine code

--- src/BinarySearch.c
+++ src/BinarySearch.c
@@ -7,21 +7,22 @@
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
+#include<conio.h>
 
 int main(void) {
 
 	int c, first, last, middle, n, search, array[100];
+	int i, j, temp;
 
 	printf("enter the number of the element.\n");
 	scanf("%d", &n);
-	printf("enter the number: %d", n);
 
+	// Input the number
 	for(c = 0; c < n; ++c) {
+		printf("input the %d number: ", c+1);
 		scanf("%d", &array[c]);
+		printf("\n");
 	}
 
 	printf("Enter the value to find\n");
@@ -29,37 +30,47 @@ int main(void) {
 
 	first = 0;
 	last = n-1;
-	middle = (first + last)/2;
 
-	while(first < last) {
+    // Use bubble sort to sort by from lower to high
+    for (j = n-1; j > 0; j--) {
+    	for (i = 0; i < j; i++) {
+    		if (array[i] > array[i+1]) {
+    			temp = array[i];
+    			array[i] = array[i+1];
+    			array[i+1] = temp;
+    		}
+    		printf("array[%d] = %d ", i, array[i]);
+    	}
+    	printf("\n");
+    }
+
+    printf("The array data afeter sorting is as following:\n");
+    for (i = 0; i < n; i++) {
+    	printf("array[%d] = %d ", i, array[i]);
+    }
+
+    printf("\n");
+
+	// Binary search operation
+	while(first <= last) {
+
+		middle = (first + last)/2;
 
 		if(array[middle] < search) {
 			first =  middle + 1;
 		}
-		else if(array[middle] == search) {
-			printf("%d found at location %d\n", search, middle+1);
+		else if (array[middle] == search) {
+			printf("The number %d in the %d position\n", search, middle+1);
 			break;
 		}
 		else {
 			last = middle - 1;
 		}
 
-		middle = (first + last)/2;
 	}
 
-	if(first > last) {
-		printf("not found\n");
-	}
+	printf("please press key to exit.\n");
+	while(!kbhit());
 
-	while(1) {
-
-		if(kbhit()) {
-			break;
-		}
-
-	}
-
-
-//	puts("!!!Hello Doremi!!!"); /* prints !!!Hello Doremi!!! */
-	return EXIT_SUCCESS;
+	return 0;
 }
